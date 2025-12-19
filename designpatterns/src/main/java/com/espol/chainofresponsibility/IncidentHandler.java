bcc81cbe4ead3d1807bea25a08e0da8935396bbe
LenierGlez <leniprie@espol.edu.ec>
2025-12-19 16:56:51 -0500

    Agregar Chain of Responsibility

---
+++ designpatterns/src/main/java/com/espol/chainofresponsibility/IncidentHandler.java
@@ -0,0 +1,21 @@
+package com.espol.chainofresponsibility;
+
+public abstract class IncidentHandler {
+    protected IncidentHandler nextHandler;
+    
+    public IncidentHandler setNext(IncidentHandler handler) {
+        this.nextHandler = handler;
+        return handler;
+    }
+    
+    public void handleIncident(Incident incident) {
+        if (canHandle(incident)) {
+            process(incident);
+        } else if (nextHandler != null) {
+            nextHandler.handleIncident(incident);
+        }
+    }
+    
+    protected abstract boolean canHandle(Incident incident);
+    protected abstract void process(Incident incident);
+}
